bf28a4e23763c691a381abe508fe8067ad91549e
dekux-x <saidshabekov@gmail.com>
2024-04-21 14:25:09 +0500

    final version

--- dogs/tests.py
+++ dogs/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestDogs(TestCase):
+    
+    def test_index_get(self):
+        response = self.client.get('/dogs/')
+        self.assertEqual(200, response.status_code)
+    
+    def test_index_post(self):
+        response = self.client.post('/dogs/')
+        self.assertEqual(400,response.status_code)
+    
+    def test_index_put(self):
+        response = self.client.put('/dogs/')
+        self.assertEqual(400,response.status_code)
+    
+    def test_index_patch(self):
+        response = self.client.patch('/dogs/')
+        self.assertEqual(400,response.status_code)
+
+    def test_detail_get(self):
+        response = self.client.get('/dogs/australian/')
+        self.assertEqual(200, response.status_code)
+    
+    def test_detail_breed(self):
+        response = self.client.get("/dogs/sldfj/")
+        self.assertEqual(404,response.status_code)
+
+    def test_detail_post(self):
+        response = self.client.post('/dogs/australian/')
+        self.assertEqual(400,response.status_code)
+    
+    def test_detail_put(self):
+        response = self.client.put('/dogs/australian/')
+        self.assertEqual(400,response.status_code)
+    
+    def test_detail_patch(self):
+        response = self.client.patch('/dogs/australian/')
+        self.assertEqual(400,response.status_code)
+
+    def test_index_content(self):
+        response = self.client.get("/dogs/")
+        self.assertIn("ILoveDogs", response.content.decode())
+    
+    def test_detail_content(self):
+        response = self.client.get("/dogs/australian/")
+        self.assertIn("australian", response.content.decode())
+        
+
--- dogs/views.py
+++ dogs/views.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render
 import requests
 from django.http import JsonResponse, HttpResponse, Http404
-from django.core.paginator import Paginator
+from django.core.exceptions import BadRequest
 
 # Create your views here.
 def index(request):
-    page = int(request.GET.get('page')) if request.GET.get('page') != None else 1
-    offset = 10
-    start = (page - 1) * offset
-    end = page * offset
-    catalog = []
-    dogs = list(get_dogs())
-    if start > len(dogs):
-        return Http404()
-    for i in dogs[start:end]:
-        image = get_random(i)
-        catalog.append([i,image])
-    previous = None if page==1 else page -1
-    next_p = None if page * offset >= len(dogs) else page + 1
-    return render(request, "dogs/index.html", {"catalog": catalog,"dogs": dogs ,"previous": previous, "next": next_p})
+    if request.method == "GET":
+        page = int(request.GET.get('page')) if request.GET.get('page') != None else 1
+        offset = 10
+        start = (page - 1) * offset
+        end = page * offset
+        catalog = []
+        dogs = list(get_dogs())
+        if start > len(dogs) or start < 0:
+            raise BadRequest("Page doesn't exist")
+        for i in dogs[start:end]:
+            image = get_random(i)
+            catalog.append([i,image])
+        previous = None if page==1 else page -1
+        next_p = None if page * offset >= len(dogs) else page + 1
+        return render(request, "dogs/index.html", {"catalog": catalog,"dogs": dogs ,"previous": previous, "next": next_p})
+    else:
+        raise BadRequest("Bad method")
+    
 
 def detail(request, breed):
-    images = get_dog(breed)
-    dogs = get_dogs()
-    return render(request, "dogs/detail.html", {"breed": breed , "images": images, "dogs": dogs})
+    if request.method == "GET":
+        images = get_dog(breed)
+        if images == "Breed not found (master breed does not exist)":
+            raise Http404(	"Breed not found (master breed does not exist)")
+        dogs = get_dogs()
+        return render(request, "dogs/detail.html", {"breed": breed , "images": images, "dogs": dogs})
+    else:
+        raise BadRequest("Bad method")
     
 
 def get_dogs():
